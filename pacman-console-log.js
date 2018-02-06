4eebb152a7c1d9ea24bb8a369c472aac1a23e5b5
vioif <ireneaflores3@gmail.com>
2018-02-06 15:42:56 -0500

    completed assignment

--- pacman-console-log.js
+++ pacman-console-log.js
@@ -1,5 +1,4 @@
 // pacman-console-log.js
-
 console.log('Pac-Man eats ghost for lunch.');
 
 // pacman-arrays.js
@@ -33,3 +32,84 @@ var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
 for (var index = 0; index < ghosts.length; index++) {
   console.log('Pac-Man eats ' + ghosts[index]);
 }
+
+// pacman-if-else.js
+var powerPelletEaten = false;
+var ghosts = 4;
+
+while (true) {
+  console.log('Ghost remaining: ' + ghosts);
+
+  if (powerPelletEaten == false) {
+    console.log('Pac-Man eats the power pellet.');
+    powerPelletEaten = true;
+  } else if (ghosts > 0) {
+    console.log('Pac-Man eats a ghost.');
+    ghosts--;
+  } else {
+    break;
+  }
+}
+
+console.log('Pac-Man returns home after a long day in the maze.');
+
+// pacman-switch.js
+var ghost = 'Blinky';
+var colour;
+
+switch (ghost) {
+  case 'Inky':
+    colour = 'Cyan';
+    break;
+  case 'Blinky':
+    colour = 'Red';
+    break;
+  case 'Pinky':
+    colour = 'Pink';
+    break;
+  case 'Clyde':
+    colour = 'Orange';
+    break;
+}
+
+console.log(ghost + ' is the colour ' + colour);
+
+// Named function
+function eatDot() {
+  console.log("MUNCH!");
+}
+
+// Anonymous function
+// Here we assign the function to a variable instead of giving it a name directly
+var eatDot = function() {
+  console.log("MUNCH!");
+}
+
+// pacman-return.js
+function addScore(currentScore, eaten) {
+  switch (eaten) {
+  case 'dot':
+    scoreToAdd = 10;
+    break;
+  case 'powerPellet':
+    scoreToAdd = 50;
+    break;
+  case 'firstGhost':
+    scoreToAdd = 200;
+    break;
+  case 'secondGhost':
+    scoreToAdd = 400;
+    break;
+  }
+
+  return currentScore + scoreToAdd;
+}
+
+var score = 0;
+
+score = addScore(score, 'dot');
+score = addScore(score, 'dot');
+score = addScore(score, 'dot');
+score = addScore(score, 'powerPellet');
+score = addScore(score, 'firstGhost');
+console.log('Your score: ' + score);
